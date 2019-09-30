428949cb9b4712f4bb89e6a8a1a6a481ece54607
Igor Patrick Ponticelli <igor_p_ponticelli@estudante.sc.senai.br>
2019-09-30 19:52:14 -0300

    Salvar........
    Garantir

--- index.php
+++ index.php
@@ -86,13 +86,11 @@ Abra a URL http://localhost/login no navegador -->
                     </div>
 
                     <div class="form-group">
-                        <input type="email" name="emailGerarSenha" id="emailGerarSenha" class="form-control" 
-                        placeholder="E-mail de recuperação de senha">
+                        <input type="email" name="emailGerarSenha" id="emailGerarSenha" class="form-control" placeholder="E-mail de recuperação de senha">
                     </div>
 
                     <div class="form-group">
-                        <input type="submit" value="Gerar" name="btnGerar" id="btnGerar" 
-                        class="btn btn-primary btn-block">
+                        <input type="submit" value="Gerar" name="btnGerar" id="btnGerar" class="btn btn-primary btn-block">
                     </div>
 
                     <div class="form-group">
@@ -108,9 +106,41 @@ Abra a URL http://localhost/login no navegador -->
         <!-- Final da Seção de Recuperação de Senha -->
         <!-- Formulário de Novo Usuário -->
         <section>
-        
+
         </section>
         <!-- Final da Seção de Novo Usuário -->
+        <!-- Início do formulário de cadastro de novos usuários -->
+        <section class="row mt-5">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
+                <h2 class="text-center mt-2">
+                    Registra-se aqui!
+                </h2>
+                <form action="#" method="post" class="p-2" id="formRegistro">
+
+                    <div class="form-group">
+                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" 
+                        placeholder="Nome Completo">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" 
+                        placeholder="Nome de Usuário">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" 
+                        placeholder="E-mail">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control"
+                        placeholder="Senha">
+                    </div>
+
+                </form>
+            </div>
+        </section>
+        <!-- Final do formulário de cadastro de novos usuários -->
     </main>
 
     <!-- Optional JavaScript -->
