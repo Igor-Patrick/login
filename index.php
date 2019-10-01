3c46301594f4b4c9b93bc12ff7a3d9e26fbd55cd
Igor Patrick Ponticelli <igor_p_ponticelli@estudante.sc.senai.br>
2019-09-30 21:17:49 -0300

    Formulário de cadastro de novo usuário
    Close #3

--- index.php
+++ index.php
@@ -12,6 +12,11 @@ Abra a URL http://localhost/login no navegador -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Sistema de Login</title>
+    <style>
+        #alerta, #caixaSenha, #caixaRegistro {
+            display: none;
+        }
+    </style>
 </head>
 
 <body class="bg-dark ">
@@ -62,7 +67,7 @@ Abra a URL http://localhost/login no navegador -->
 
                     <div class="form-group">
                         <p class="text-center">
-                            Novo usuário? <a href="#" id="btnRegistrar">
+                            Novo usuário? <a href="#" id="btnRegistrarNovo">
                                 Registre-se aqui!
                             </a>
                         </p>
@@ -74,7 +79,7 @@ Abra a URL http://localhost/login no navegador -->
         <!-- Final da Seção de Login -->
         <!-- Formulário de Recuperação de Senha -->
         <section class="row mt-5">
-            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
                 <h2 class="text-center mt-2">
                     Gerar Nova Senha
                 </h2>
@@ -146,6 +151,18 @@ Abra a URL http://localhost/login no navegador -->
                         </div>
                     </div>
 
+                    <div class="form-group">
+                        <input type="submit" value="Registrar" class="btn btn-primary btn-block" id="btnRegistrar" name="btnRegistrar">
+                    </div>
+
+                    <div class="form-group">
+                        <p class="text-center">
+                            Já registrado? <a href="#" id="btnJaRegistrado2">
+                                Entrar por aqui!
+                            </a>
+                        </p>
+                    </div>
+
                 </form>
             </div>
         </section>
@@ -157,6 +174,9 @@ Abra a URL http://localhost/login no navegador -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+    // Código jQuery para mostrar e ocultar os formulários
+    </script>
 </body>
 
 </html>
