7b22e69aa1ea616bcbe1e2b28a041604423e9805
Igor Patrick Ponticelli <igor_p_ponticelli@estudante.sc.senai.br>
2019-11-19 01:20:18 -0200

    Atualização do Banco de Dados
    Inclusão dos campos token e Tempo de Vida.

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Out-2019 às 21:26
+-- Tempo de geração: 19-Nov-2019 às 01:14
 -- Versão do servidor: 10.3.16-MariaDB
 -- versão do PHP: 7.3.7
 
@@ -33,14 +33,25 @@ USE `senai`;
 
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
-  `idUsuario` int(10) UNSIGNED NOT NULL PRIMARY KEY auto_increment,
+  `idUsuario` int(10) UNSIGNED NOT NULL,
   `nomeCompleto` varchar(100) NOT NULL,
   `nomeDoUsuario` varchar(45) NOT NULL,
   `emailUsuario` varchar(45) NOT NULL,
   `senhaDoUsuario` char(40) NOT NULL,
   `dataCriado` date NOT NULL,
-  `urlDePerfil`	varchar(200) NOT NULL
+  `urlDePerfil` varchar(200) NOT NULL,
+  `token` char(10) NOT NULL,
+  `tempoDeVida` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `nomeCompleto`, `nomeDoUsuario`, `emailUsuario`, `senhaDoUsuario`, `dataCriado`, `urlDePerfil`, `token`, `tempoDeVida`) VALUES
+(1, 'patrick', 'patrick', 'patrick@patrick', 'cbb7353e6d953ef360baf960c122346276c6e320', '2019-11-19', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE3rNU_E9AYU2RAc3r2Vz90WemLu9-0BpAeCIUefSGZljHSfJN&amp;s', '', '2019-11-19 03:07:53'),
+(2, 'ponticelli', 'ponticelli', 'ponticelli@ponticelli', '7fe51f557677a85221b2436be9ff9635559a3a70', '2019-11-19', 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/slideshows/more_potassium_banana_slideshow/1800x1200_more_potassium_banana_slideshow.jpg', '', '2019-11-19 03:07:53'),
+(3, 'timestamp', 'timestamp', 'timestamp@timestamp', '601f1889667efaebb33b8c12572835da3f027f78', '2019-11-19', 'https://i.pinimg.com/originals/de/f6/96/def69643889ee29e232637646e839064.jpg', '', '2019-11-19 03:11:48');
 
 --
 -- Índices para tabelas despejadas
@@ -50,8 +61,19 @@ CREATE TABLE `usuario` (
 -- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
   ADD UNIQUE KEY `nomeDoUsuario` (`nomeDoUsuario`),
   ADD UNIQUE KEY `emailUsuario` (`emailUsuario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
